7be16ebf8a56d65fe544be78075ea9b482d5b1ca
ilkukrisz <ilku355@gmail.com>
2018-10-10 11:24:09 +0200

    LibrarianBookService and ReaderBookService had been created. BookService had been modified by the requested conditions.

---
+++ core/src/main/java/hu/uni/miskolc/iit/softwaretesting/service/LibrarianBookService.java
@@ -0,0 +1,81 @@
+package hu.uni.miskolc.iit.softwaretesting.service;
+
+import hu.uni.miskolc.iit.softwaretesting.exceptions.*;
+import hu.uni.miskolc.iit.softwaretesting.model.Book;
+
+import java.util.Collection;
+
+public interface LibrarianBookService extends BookService {
+
+    /**
+     *
+     * @param book - The book which should be added to the database
+     * @throws AlreadyExistingBookException
+     * @throws WrongISBNException
+     * @throws EmptyFieldException
+     * @throws InvalidPublishDateException
+     * @throws NotExistingGenreException
+     */
+    public void addBook(Book book) throws AlreadyExistingBookException, WrongISBNException, EmptyFieldException, InvalidPublishDateException, NotExistingGenreException;
+
+    /**
+     *
+     * @param book - The book which should be updated.
+     * @throws WrongISBNException
+     * @throws EmptyFieldException
+     * @throws InvalidPublishDateException
+     * @throws NotExistingGenreException
+     */
+    void updateBook(Book book) throws WrongISBNException, EmptyFieldException, InvalidPublishDateException, NotExistingGenreException;
+
+    /**
+     *
+     * @param id the id of the book, which should be deleted.
+     * @throws BookNotFoundException
+     */
+    void deleteBook(int id) throws BookNotFoundException;
+
+    /**
+     *
+     * @return the number of books stored in the database.
+     */
+    int countBooks();
+
+
+    /**
+     *
+     * @param bookInstance the new instance which should be added to the database
+     * @throws AlreadyExistingBookInstance
+     * @throws EmptyFieldException
+     */
+    public void addBookInstances(BookInstance bookInstance) throws AlreadyExistingBookInstance, EmptyFieldException;
+
+    /**
+     *
+     * @param inventoryNumber the book instance that should be deleted from the databse.
+     */
+    public void deleteBookInstances(long inventoryNumber);
+
+    /**
+     *
+     * @param borrow the borrow which will be examined and completed
+     * @throws NotExistingBorrowing
+     */
+    public void lendBook(Borrowing borrow) throws NotExistingBorrowing;
+
+    /**
+     *
+     * @return the list of borrowings
+     */
+    public Collection<Borrowing> listBorrowings();
+
+    /**
+     *
+     * @return the list of requests
+     */
+    public Collection<Borrowing> listRequests();
+
+
+
+
+}
