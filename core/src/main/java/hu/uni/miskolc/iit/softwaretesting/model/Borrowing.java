e1dfcf1e3036125c49ff942cd277a901d8d65f62
Kiss, Aron <aron123dev@gmail.com>
2018-10-11 11:13:56 +0200

    Add models related to borrowing
    BookInstance, Borrowing, BorrowStatus

---
+++ core/src/main/java/hu/uni/miskolc/iit/softwaretesting/model/Borrowing.java
@@ -0,0 +1,108 @@
+package hu.uni.miskolc.iit.softwaretesting.model;
+
+import java.util.Date;
+
+/**
+ * Represents a book borrowing.
+ */
+public class Borrowing {
+    /**
+     * The ID of the borrowing.
+     */
+    long borrowID;
+
+    /**
+     * The reader, who borrows.
+     */
+    Reader reader;
+
+    /**
+     * The creation date of the borrowing.
+     */
+    Date creationDate;
+
+    /**
+     * The expiration of the borrowing.
+     */
+    Date expirationDate;
+
+    /**
+     * The status of the Borrowing.
+     * @see BorrowStatus
+     */
+    BorrowStatus status;
+
+    /**
+     * The borrowed book instance.
+     */
+    BookInstance bookInstance;
+
+    public Borrowing(long borrowID, Reader reader, Date creationDate, Date expirationDate, BorrowStatus status, BookInstance bookInstance) {
+        this.borrowID = borrowID;
+        this.reader = reader;
+        this.creationDate = creationDate;
+        this.expirationDate = expirationDate;
+        this.status = status;
+        this.bookInstance = bookInstance;
+    }
+
+    public long getBorrowID() {
+        return borrowID;
+    }
+
+    public void setBorrowID(long borrowID) {
+        this.borrowID = borrowID;
+    }
+
+    public Reader getReader() {
+        return reader;
+    }
+
+    public void setReader(Reader reader) {
+        this.reader = reader;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public BorrowStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(BorrowStatus status) {
+        this.status = status;
+    }
+
+    public BookInstance getBookInstance() {
+        return bookInstance;
+    }
+
+    public void setBookInstance(BookInstance bookInstance) {
+        this.bookInstance = bookInstance;
+    }
+
+    @Override
+    public String toString() {
+        return "Borrowing{" +
+                "borrowID=" + borrowID +
+                ", reader=" + reader +
+                ", creationDate=" + creationDate +
+                ", expirationDate=" + expirationDate +
+                ", status=" + status +
+                ", bookInstance=" + bookInstance +
+                '}';
+    }
+}
