d5d8e7bf7de58e66ae7263219f6e225ad9d82d31
Hega-PC <hegedusrendszer@gmail.com>
2018-10-15 12:39:11 +0200

    Problem fixed, 2 more classes added.

---
+++ core/src/main/java/hu/uni/miskolc/iit/softwaretesting/model/Reader.java
@@ -0,0 +1,9 @@
+package hu.uni.miskolc.iit.softwaretesting.model;
+
+public class Reader extends User {
+
+    public Reader(String username, String password, String firstName, String lastName, String email, String mobileNumber) {
+        super(username, password, firstName, lastName, email, mobileNumber);
+    }
+}
+
