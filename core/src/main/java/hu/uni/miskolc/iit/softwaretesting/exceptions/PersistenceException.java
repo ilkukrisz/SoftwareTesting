2c8606495f7076af5b5fffda653585e85fe2d6b4
Kiss, Aron <aron123dev@gmail.com>
2018-11-06 12:21:49 +0100

    Add PersistenceException class

---
+++ core/src/main/java/hu/uni/miskolc/iit/softwaretesting/exceptions/PersistenceException.java
@@ -0,0 +1,25 @@
+package hu.uni.miskolc.iit.softwaretesting.exceptions;
+
+/**
+ * A technology-independent exception, that is used when error occurs while manipulating database.
+ */
+public class PersistenceException extends Exception {
+    public PersistenceException() {
+    }
+
+    public PersistenceException(String message) {
+        super(message);
+    }
+
+    public PersistenceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PersistenceException(Throwable cause) {
+        super(cause);
+    }
+
+    public PersistenceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
