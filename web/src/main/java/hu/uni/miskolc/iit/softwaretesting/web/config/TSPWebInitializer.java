9f981ee5076ef06770aca8eb79f9608554bfdf50
Kiss, Aron <aron123dev@gmail.com>
2018-11-10 17:38:12 +0100

    Add server initialization

---
+++ web/src/main/java/hu/uni/miskolc/iit/softwaretesting/web/config/TSPWebInitializer.java
@@ -0,0 +1,27 @@
+package hu.uni.miskolc.iit.softwaretesting.web.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class TSPWebInitializer implements WebApplicationInitializer {
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext annotationConfigCtx = new AnnotationConfigWebApplicationContext();
+
+        annotationConfigCtx.register(LibrarianContext.class);
+        annotationConfigCtx.register(ReaderContext.class);
+        annotationConfigCtx.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servletRegistration = servletContext.addServlet(
+                "dispatcher", new DispatcherServlet((WebApplicationContext) annotationConfigCtx)
+        );
+
+        servletRegistration.setLoadOnStartup(1);
+        servletRegistration.addMapping("/");
+    }
+}
